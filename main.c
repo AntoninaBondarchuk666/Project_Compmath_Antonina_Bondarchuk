70867b6e71da1d19aa2335fbd939b97d0550f5d6
AntoninaBondarchuk666 <58553305+AntoninaBondarchuk666@users.noreply.github.com>
2019-12-16 21:12:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,25 +13,38 @@ int main() {
     printf("Please, enter the second Big Float number:  \n");
     char line2[1024];
     scanf("%1023[^\n]", line2);
+    getchar();
+    printf("Please, enter the third Big Float number:  \n");
+    char line3[1024];
+    scanf("%1023[^\n]", line3);
+    printf("\n");
     
-    BigFloat *X, *Y, *res1, *res2;
+    BigFloat *X, *Y, *Z, *res1, *res2;
    
     X = create(line1); 
     Y = create(line2);
+    Z = create(line3);
     
     res1 = create(d);
     res2 = create(d);
     
     add(X,Y,res1);
-    multiply(X,Y,res2);
+    multiply(X,Y,res1);
+    solve_for_y(X,Y,Z,res1,res2);
     
     printf("Sum of two Big Float numbers:  \n");
     print(res1);
+    printf("\n");
     printf("Multiplication of two Big Float numbers:  \n");
+    print(res1);
+    printf("\n");
+    printf("Solve the Linear Equation in One Variable for three Big Float numbers:  \n");
     print(res2);
+    printf("\n");
     
     freeBigFloat(X);
     freeBigFloat(Y);
+    freeBigFloat(Z);
     freeBigFloat(res1);
     freeBigFloat(res2);
     return 0;
